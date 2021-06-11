b2aeedb8bf15d08454d093a13d1f2dcb76c892dc
Lucas Yang <yangchenshin77@gmail.com>
2021-06-11 18:04:29 +0800

    Add base packages

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view />
+  <div>
+    Home
+  </div>
 </template>
 
 <script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pages from 'vite-plugin-pages'
+import icons from 'vite-plugin-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    pages(),
+    icons(),
+  ],
 })
