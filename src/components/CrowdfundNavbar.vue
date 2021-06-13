e2a092047cd59349f5dc4b04ce289180b06d3aad
Lucas Yang <yangchenshin77@gmail.com>
2021-06-14 02:52:53 +0800

    Use "exact-active-class"

--- src/components/CrowdfundNavbar.vue
+++ src/components/CrowdfundNavbar.vue
@@ -4,22 +4,22 @@
       <nav class="-mx-3 sm:mx-0 lg:mx-2.5">
         <ul class="flex lg:space-x-4">
           <li>
-            <router-link to="/" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" :class="isActive('/') ? 'font-bold border-b-2 border-yellow' : ''">
+            <router-link to="/" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" exact-active-class="font-bold border-b-2 border-yellow">
               專案介紹
             </router-link>
           </li>
           <li>
-            <router-link to="/faqs" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" :class="isActive('/faqs') ? 'font-bold border-b-2 border-yellow' : ''">
+            <router-link to="/faqs" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" exact-active-class="font-bold border-b-2 border-yellow">
               常見問答
             </router-link>
           </li>
           <li>
-            <router-link to="/news" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" :class="isActive('/news') ? 'font-bold border-b-2 border-yellow' : ''">
+            <router-link to="/news" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" exact-active-class="font-bold border-b-2 border-yellow">
               目前進度
             </router-link>
           </li>
           <li>
-            <router-link to="/comments" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" :class="isActive('/comments') ? 'font-bold border-b-2 border-yellow' : ''">
+            <router-link to="/comments" class="block p-4 lg:px-3 lg:py-6 text-gray-600 text-sm lg:text-base tracking-[1.75px]" exact-active-class="font-bold border-b-2 border-yellow">
               留言
             </router-link>
           </li>
@@ -28,13 +28,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    isActive(path) {
-      return this.$route.path === path
-    },
-  },
-}
-</script>
