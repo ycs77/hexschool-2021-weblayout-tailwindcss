05e598d97110aed6fecff0e9285728b7be640b84
Lucas Yang <yangchenshin77@gmail.com>
2021-06-14 04:06:57 +0800

    Sticky sidebar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -29,18 +29,20 @@
         </div>
 
         <div class="lg:col-span-1">
-          <CrowdfundCreatorInfo />
+          <div class="sticky top-[108px]">
+            <CrowdfundCreatorInfo />
 
-          <div class="mt-8">
-            <h3 class="bg-gray-50 py-1.5 text-xl text-center font-bold border-l-4 border-r-4 border-yellow">
-              選擇一個方案來支持
-            </h3>
+            <div class="mt-8">
+              <h3 class="bg-gray-50 py-1.5 text-xl text-center font-bold border-l-4 border-r-4 border-yellow">
+                選擇一個方案來支持
+              </h3>
 
-            <ul class="mt-4 space-y-4">
-              <li v-for="i in 3">
-                <CrowdfundCard />
-              </li>
-            </ul>
+              <ul class="mt-4 space-y-4">
+                <li v-for="i in 3">
+                  <CrowdfundCard />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
