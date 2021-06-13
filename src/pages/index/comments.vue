c4b3403d62be0fb687655320550499d86ddc2df6
Lucas Yang <yangchenshin77@gmail.com>
2021-06-13 23:33:04 +0800

    Comments page

--- src/pages/index/comments.vue
+++ src/pages/index/comments.vue
@@ -1,3 +1,88 @@
 <template>
-  <div>comments</div>
+  <div class="mb-8 p-4 bg-gray-50 text-sm tracking-[0.7px] border-l-3 border-yellow rounded">
+    只有本專案的贊助者才可以留言哦，如果有任何專案相關的問題，歡迎<a class="text-red hover:text-light-red" href="#contact" @click.prevent>聯絡提案人</a>！
+  </div>
+
+  <ul class="space-y-4">
+    <li class="p-4 border border-gray-100 rounded-lg">
+      <div class="flex items-center">
+        <img class="w-12 h-12 lg:w-[60px] lg:h-[60px] rounded-full" src="../../assets/user_img01.jpg" alt="廖小杰 的大頭照">
+        <div class="ml-3.5 lg:ml-4">
+          <div class="lg:text-xl font-medium tracking-[1.6px] lg:tracking-[2px]">
+            廖小杰
+          </div>
+          <div class="lg:mt-1 text-gray-500 text-xs lg:text-sm tracking-[1.71px] leading-[18px]">
+            2020年5月22日 11:32
+          </div>
+        </div>
+      </div>
+
+      <p class="mt-2 lg:mt-4 text-gray-900 text-justify tracking-[0.7px]">
+        晚上起床上廁所看到照片裡的人一直動其實有點恐怖，希望可以有暫停或是定時的功能！
+      </p>
+
+      <div class="p-4 mt-2 lg:mt-4 bg-gray-50">
+        <div class="text-xs lg:text-sm text-red tracking-[0.53px]">
+          提案者回覆
+        </div>
+        <p class="mt-1 lg:mt-0 text-gray-900 text-justify tracking-[0.7px]">
+          你要嘛起床的時候開燈，要嘛給我們更多錢開發阿
+        </p>
+      </div>
+    </li>
+
+    <li class="p-4 border border-gray-100 rounded-lg">
+      <div class="flex items-center">
+        <img class="w-12 h-12 lg:w-[60px] lg:h-[60px] rounded-full" src="../../assets/user_img02.jpg" alt="卡阿伯 的大頭照">
+        <div class="ml-3.5 lg:ml-4">
+          <div class="lg:text-xl font-medium tracking-[1.6px] lg:tracking-[2px]">
+            卡阿伯
+          </div>
+          <div class="lg:mt-1 text-gray-500 text-xs lg:text-sm tracking-[1.71px] leading-[18px]">
+            2020年5月22日 11:32
+          </div>
+        </div>
+      </div>
+
+      <p class="mt-2 lg:mt-4 text-gray-900 text-justify tracking-[0.7px]">
+        希望有更多花色可以選擇！我女兒最近喜歡綠色
+      </p>
+    </li>
+
+    <li class="p-4 border border-gray-100 rounded-lg">
+      <div class="flex items-center">
+        <img class="w-12 h-12 lg:w-[60px] lg:h-[60px] rounded-full" src="../../assets/user_img02.jpg" alt="賈師 的大頭照">
+        <div class="ml-3.5 lg:ml-4">
+          <div class="lg:text-xl font-medium tracking-[1.6px] lg:tracking-[2px]">
+            賈師
+          </div>
+          <div class="lg:mt-1 text-gray-500 text-xs lg:text-sm tracking-[1.71px] leading-[18px]">
+            2020年5月22日 11:32
+          </div>
+        </div>
+      </div>
+
+      <p class="mt-2 lg:mt-4 text-gray-900 text-justify tracking-[0.7px]">
+        可以做個相簿功能嗎？拍出好看的影片再讓我們選擇要印出哪一張這樣
+      </p>
+    </li>
+
+    <li class="p-4 border border-gray-100 rounded-lg">
+      <div class="flex items-center">
+        <img class="w-12 h-12 lg:w-[60px] lg:h-[60px] rounded-full" src="../../assets/user_img02.jpg" alt="俊俊 的大頭照">
+        <div class="ml-3.5 lg:ml-4">
+          <div class="lg:text-xl font-medium tracking-[1.6px] lg:tracking-[2px]">
+            Lucas
+          </div>
+          <div class="lg:mt-1 text-gray-500 text-xs lg:text-sm tracking-[1.71px] leading-[18px]">
+            2020年5月22日 11:32
+          </div>
+        </div>
+      </div>
+
+      <p class="mt-2 lg:mt-4 text-gray-900 text-justify tracking-[0.7px]">
+        test123 看一下我是不是真的可以留言
+      </p>
+    </li>
+  </ul>
 </template>
