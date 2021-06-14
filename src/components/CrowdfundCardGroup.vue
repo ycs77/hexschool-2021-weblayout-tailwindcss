5d741cfecf27c7344cd5aebeee6edf8d4c12f468
Lucas Yang <yangchenshin77@gmail.com>
2021-06-14 17:06:46 +0800

    Extract CardGroup & refactor index page layout

---
+++ src/components/CrowdfundCardGroup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <CrowdfundCreatorInfo />
+
+    <div class="mt-8">
+      <h3 class="bg-gray-50 py-1.5 text-xl text-center font-bold border-l-4 border-r-4 border-yellow">
+        選擇一個方案來支持
+      </h3>
+
+      <ul class="mt-4 space-y-4">
+        <li v-for="i in 3">
+          <CrowdfundCard />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,50 +5,32 @@
 
     <CrowdfundNavbar class="sticky top-0 z-10 bg-white" />
 
-    <div class="container pt-8 pb-12 grid">
+    <div class="container pt-8 pb-12 grid lg:grid-cols-3 lg:gap-12">
+      <div class="lg:col-span-2">
+        <main>
+          <RouterView v-slot="{ Component }">
+            <transition
+              enter-active-class="transition duration-200 ease-out"
+              enter-from-class="transform opacity-0"
+              enter-to-class="transform opacity-100"
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="transform opacity-100"
+              leave-to-class="transform opacity-0"
+              mode="out-in"
+            >
+              <component :is="Component" />
+            </transition>
+          </RouterView>
+        </main>
 
-      <div class="grid lg:grid-cols-3 gap-8 lg:gap-12">
-        <div class="lg:col-span-2">
-          <main>
-            <RouterView v-slot="{ Component }">
-              <transition
-                enter-active-class="transition duration-200 ease-out"
-                enter-from-class="transform opacity-0"
-                enter-to-class="transform opacity-100"
-                leave-active-class="transition duration-100 ease-in"
-                leave-from-class="transform opacity-100"
-                leave-to-class="transform opacity-0"
-                mode="out-in"
-              >
-                <component :is="Component" />
-              </transition>
-            </RouterView>
-          </main>
+        <CrowdfundCardGroup class="mt-8 lg:hidden" />
 
-          <CrowdfundForm class="hidden lg:block mt-12" />
-        </div>
-
-        <div class="lg:col-span-1">
-          <div class="sticky top-[108px]">
-            <CrowdfundCreatorInfo />
-
-            <div class="mt-8">
-              <h3 class="bg-gray-50 py-1.5 text-xl text-center font-bold border-l-4 border-r-4 border-yellow">
-                選擇一個方案來支持
-              </h3>
-
-              <ul class="mt-4 space-y-4">
-                <li v-for="i in 3">
-                  <CrowdfundCard />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <CrowdfundForm ref="form" class="mt-8 lg:mt-12" />
       </div>
 
-      <CrowdfundForm ref="mobileForm" class="lg:hidden mt-8" />
-
+      <div class="hidden lg:block lg:col-span-1">
+        <CrowdfundCardGroup class="sticky top-[108px]" />
+      </div>
     </div>
 
     <transition
@@ -73,7 +55,7 @@ export default {
     return {
       showBottomBtn: false,
       inBanner: false,
-      inMobileForm: false,
+      inForm: false,
     }
   },
   methods: {
@@ -89,12 +71,12 @@ export default {
       return el.offsetTop + el.clientHeight
     },
     formTop() {
-      const el = this.$refs.mobileForm.$el
+      const el = this.$refs.form.$el
       return el ? el.offsetTop : 0
     },
 
     updateShowBottomBtnWithObserver() {
-      this.showBottomBtn = !this.inBanner && !this.inMobileForm
+      this.showBottomBtn = !this.inBanner && !this.inForm
     },
 
     observeBanner() {
@@ -107,19 +89,19 @@ export default {
       observer.observe(this.$refs.banner.$el)
     },
 
-    observeMobileForm() {
+    observeForm() {
       const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-          this.inMobileForm = entry.isIntersecting
+          this.inForm = entry.isIntersecting
           this.updateShowBottomBtnWithObserver()
         })
       }, { threshold: 0.8 })
-      observer.observe(this.$refs.mobileForm.$el)
+      observer.observe(this.$refs.form.$el)
     },
   },
   mounted() {
     this.observeBanner()
-    this.observeMobileForm()
+    this.observeForm()
   },
   beforeRouteUpdate(to, from) {
     this.scrollToNavbar()
